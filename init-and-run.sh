e39ac9e18240d6215bee6e8e77794fa0040286f8
Heiner Bunjes <heiner@bunjes.net>
2017-02-28 12:08:34 +0100

    Update init-and-run.sh

--- init-and-run.sh
+++ init-and-run.sh
@@ -1,2 +1,5 @@
 echo $label
 echo $HB_NOFLO_RUNTIME_HOST
+
+# node node_modules/.bin/noflo-nodejs-init --user $HB_FLOWHUB_USERID --host $HB_NOFLO_RUNTIME_HOST --port 3569 --label $label
+# node node_modules/.bin/noflo-nodejs                                --host $HB_NOFLO_RUNTIME_HOST --capture-output=false
